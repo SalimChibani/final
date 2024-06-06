1e82d1b5aa2dd50488cb9d700e89a264ad7fbefd
SalimChibani <163078222+SalimChibani@users.noreply.github.com>
2024-06-06 02:30:20 +0200

    Update product.js

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,26 +1,33 @@
-const router = require('express').Router();
-const multer = require('multer');
+    const router = require('express').Router();
+    const multer = require('multer');
 
-const {addProduct, getProducts, getProductById, updateProduct, deleteProduct,addImage,getProductImage} = require('../controller/product');
-const multerConfigImage = require('../config/multer');
+    const {
+        addProduct,
+        getProducts,
+        getProductById,
+        updateProduct,
+        deleteProduct,
+        addImage,
+        getProductImage
+    } = require('../controller/product');
+    const multerConfigImage = require('../config/multer');
 
-router.post('/', addProduct);
-router.get('/', getProducts);
-router.get('/:id', getProductById);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+    router.post('/', addProduct);
+    router.get('/', getProducts);
+    router.get('/:id', getProductById);
+    router.put('/:id', updateProduct);
+    router.delete('/:id', deleteProduct);
 
-router.post('/upload/:id', multer(multerConfigImage).single('file'), (req, res) => {
-    try {
-        if (res.status(200)) {
-            addImage(req, res)
+    router.post('/upload/:id', multer(multerConfigImage).single('file'), (req, res) => {
+        try {
+            if (res.status(200)) {
+                addImage(req, res);
+            }
+        } catch (err) {
+            res.status(500).json({ message: err.message });
         }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-})
+    });
 
-router.get('/get-product-img/:id', getProductImage);
+    router.get('/get-product-img/:id', getProductImage);
 
-
-module.exports = router;
+    module.exports = router;
